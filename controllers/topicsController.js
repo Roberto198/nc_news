c4377949f48abf1f31ca5804c76f7dd302eeb246
Rob Clegg <roberto198@github.com>
2019-04-23 22:20:29 +0100

    display topics functionality

--- controllers/topicsController.js
+++ controllers/topicsController.js
@@ -1,8 +1,9 @@
 const { selectAllTopics } = require('../models/topicsModels');
 
 exports.sendAllTopics = (req, res, next) => {
-	console.log('topic cont');
 	selectAllTopics().then(topics => {
-		res.status(200).send({ topics });
+		res.status(200)
+			.send({ topics })
+			.catch(next);
 	});
 };
